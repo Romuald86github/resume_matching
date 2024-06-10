cf9a8c7cdbf62a37ba8094fc2b45b07afb6ae9ef
Romuald86github <123880565+Romuald86github@users.noreply.github.com>
2024-06-10 22:32:59 +0000

    update

--- scripts/feature_extraction.py
+++ scripts/feature_extraction.py
@@ -10,13 +10,13 @@ def convert_to_tfidf(resume_texts, job_texts):
     return resume_tfidf, job_tfidf
 
 if __name__ == "__main__":
-    with open('../intermediate_data/preprocessed_resumes.txt', 'r') as f:
+    with open('intermediate_data/preprocessed_resumes.txt', 'r') as f:
         preprocessed_resumes = f.readlines()
 
-    with open('../intermediate_data/preprocessed_jobs.txt', 'r') as f:
+    with open('intermediate_data/preprocessed_jobs.txt', 'r') as f:
         preprocessed_jobs = f.readlines()
 
     resume_tfidf, job_tfidf = convert_to_tfidf(preprocessed_resumes, preprocessed_jobs)
     
-    np.save('../intermediate_data/resume_tfidf.npy', resume_tfidf.toarray())
-    np.save('../intermediate_data/job_tfidf.npy', job_tfidf.toarray())
+    np.save('intermediate_data/resume_tfidf.npy', resume_tfidf.toarray())
+    np.save('intermediate_data/job_tfidf.npy', job_tfidf.toarray())
