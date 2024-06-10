60c1b5d0476fe8366bf3be0a564a001e0839c493
Romuald86github <123880565+Romuald86github@users.noreply.github.com>
2024-06-10 22:30:46 +0000

    update

--- scripts/text_preprocessing.py
+++ scripts/text_preprocessing.py
@@ -14,19 +14,19 @@ def preprocess_text(text):
     return ' '.join(words)
 
 if __name__ == "__main__":
-    with open('../intermediate_data/resume_texts.txt', 'r') as f:
+    with open('intermediate_data/resume_texts.txt', 'r') as f:
         resume_texts = f.readlines()
 
-    with open('../intermediate_data/job_texts.txt', 'r') as f:
+    with open('intermediate_data/job_texts.txt', 'r') as f:
         job_texts = f.readlines()
 
     preprocessed_resumes = [preprocess_text(text) for text in resume_texts]
     preprocessed_jobs = [preprocess_text(text) for text in job_texts]
 
-    with open('../intermediate_data/preprocessed_resumes.txt', 'w') as f:
+    with open('intermediate_data/preprocessed_resumes.txt', 'w') as f:
         for text in preprocessed_resumes:
             f.write("%s\n" % text)
 
-    with open('../intermediate_data/preprocessed_jobs.txt', 'w') as f:
+    with open('intermediate_data/preprocessed_jobs.txt', 'w') as f:
         for text in preprocessed_jobs:
             f.write("%s\n" % text)
